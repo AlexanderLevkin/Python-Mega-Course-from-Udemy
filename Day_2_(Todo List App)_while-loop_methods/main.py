c119d78da50a7ad49f924b9cb15857f88525d3e2
AlexanderLevkin <levkin.alexan@gmail.com>
2023-04-06 22:41:42 +0300

    add modified files

--- Day_2_(Todo List App)_while-loop_methods/main.py
+++ Day_2_(Todo List App)_while-loop_methods/main.py
@@ -1,5 +1,16 @@
-user_prompt = "Enter a todo:"
+# user_prompt = "Enter a:"
+#
+# todos = []
+#
+# while True:
+#     todo1 = input(user_prompt)
+#     todo2 = todo1.capitalize()
+#     todos.append(todo2)
+#     print(todos)
 
-while 2 > 1:
-    todo1 = input(user_prompt)
-    
+password = input("Enter the password: ")
+
+while password != "pass123":
+    password = input("Enter the password: ")
+
+print("Password was correct! ")
