dc449ddc5befc97a1da92b0907f13e0f8a000b35
AlexanderLevkin <levkin.alexan@gmail.com>
2023-04-23 14:51:16 +0300

    add new day

--- Day_8_with-context-manager/Coding_exercise/head_or_tail.py
+++ Day_8_with-context-manager/Coding_exercise/head_or_tail.py
@@ -1,17 +1,34 @@
-count = 0
+# my solution:
+# while True:
+#     data = input("Throw the coin and enter head or tail here or type exit:")
+#     if data == "exit":
+#         print("Bye! Bye!")
+#         break
+#     elif data not in ["head", "tail"]:
+#         print("You typed the wrong words or characters")
+#     else:
+#         with open("content/count.txt") as file:
+#             sum_ = file.readlines()
+#             sum_.append(data + "\n")
+#             list_of_head = [item for item in sum_ if item.strip() == "head"]
+#         with open("content/count.txt", "w") as file:
+#             file.writelines(sum_)
+#         final_output = len(list_of_head) / len(sum_) * 100
+#         print(f"Heads: {final_output}")
+
+# Ardit solution
 while True:
-    data = input("Throw the coin and enter head or tail here:") + "\n"
-    with open("content/sum.txt") as file:
-        content = file.readlines()
-        content.append(data)
-    with open("content/sum.txt", "w") as file:
-        file.writelines(content)
-    for item in content:
-        match item.strip():
-            case "head":
-                count += 1
-            case "":
-                count -= 1
-    final_count = count / len(content) * 100
-    print(count)
-    print(f"Heads: {final_count}")
+    with open("sides.txt", 'r') as file:
+        sides = file.readlines()
+
+    side = input("Throw the coin and enter head or tail here: ?") + "\n"
+
+    sides.append(side)
+
+    with open("sides.txt", 'w') as file:
+        file.writelines(sides)
+
+    nr_heads = sides.count("head\n")
+    percentage = nr_heads / len(sides) * 100
+
+    print(f"Heads: {percentage}%")
