ed9f51f1faade68d84ffed2a6684d3afb1d8efbb
AlexanderLevkin <levkin.alexan@gmail.com>
2023-04-26 09:16:15 +0300

    add new day

---
+++ Day_11_custom-functions/main.py
@@ -0,0 +1,65 @@
+def get_todos():
+    with open('files/todos.txt', 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+while True:
+    user_action = input("Type add, show, edit or exit: ")
+    user_action = user_action.strip()
+
+    if user_action.startswith("add"):
+        todo = user_action[4:]
+
+        todos = get_todos()
+
+        todos.append(todo + "\n")
+
+        with open('files/todos.txt', 'w') as file:
+            file.writelines(todos)
+
+    elif user_action.startswith('show'):
+
+        todos = get_todos()
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
+            print(row)
+
+    elif user_action.startswith('edit'):
+        try:
+            number = user_action[5:]
+            number = int(number) - 1
+
+            todos = get_todos()
+
+            new_todo = input("Enter the new type: ")
+            todos[number] = new_todo + '\n'
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Your command is not valid.")
+        continue
+    elif user_action.startswith('exit'):
+        break
+    elif user_action.startswith('complete'):
+        try:
+            number = user_action[9:]
+            todos = get_todos()
+
+            todo_to_remove = todos[int(number) - 1].strip('\n')
+            todos.pop(int(todo_to_remove))
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+
+            message = f'Todo {todo_to_remove} was remove from the list'
+            print(message)
+        except IndexError:
+            print("There is no item with that number.")
+        continue
+    else:
+        print("Hey! this a wrong word, please try again")
+
+print("Bye!")
