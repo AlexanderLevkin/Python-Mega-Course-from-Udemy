41386ccbb6949bd06c68f861e833f914eb281bf0
Alexander_Levkin <levkin.a@promail366.com>
2023-08-22 22:24:25 +0300

    add new 33 day

--- Day_32_and_33_Build_a_Weather_Forecast_Dashboard_with_Python_Part_1/main.py
+++ Day_32_and_33_Build_a_Weather_Forecast_Dashboard_with_Python_Part_1/main.py
@@ -12,25 +12,27 @@ option = st.selectbox(
 )
 
 st.subheader(f"{option} Forecast for {days} days in {place}")
+try:
+    if place:
+        # Get temperature and sky data
+        filtered_data = get_data(place, days)
 
-if place:
-    # Get temperature and sky data
-    filtered_data = get_data(place, days)
+        if option == 'Temperature':
+            temperature = [item['main']['temp'] / 10 for item in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+        if option == 'Sky':
+            data = [item['weather'][0]['main'] for item in filtered_data]
 
-    if option == 'Temperature':
-        temperature = [float(item['main']['temp'])-32 / 1.8 for item in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        # Create a temperature plot
-        figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y": "Temperature (C)"})
-        st.plotly_chart(figure)
-    if option == 'Sky':
-        data = [item['weather'][0]['main'] for item in filtered_data]
-
-        sky_conditions = {
-            'Clear': 'Day_32_and_33_Build_a_Weather_Forecast_Dashboard_with_Python_Part_1/img/clear.png',
-            'Clouds': 'Day_32_and_33_Build_a_Weather_Forecast_Dashboard_with_Python_Part_1/img/cloud.png',
-            'Rain': 'Day_32_and_33_Build_a_Weather_Forecast_Dashboard_with_Python_Part_1/img/rain.png',
-            'Snow': 'Day_32_and_33_Build_a_Weather_Forecast_Dashboard_with_Python_Part_1/img/snow.png'
-        }
-        pictures = [sky_conditions[item] for item in data]
-        st.image(pictures, width=115)
+            sky_conditions = {
+                'Clear': 'Day_32_and_33_Build_a_Weather_Forecast_Dashboard_with_Python_Part_1/img/clear.png',
+                'Clouds': 'Day_32_and_33_Build_a_Weather_Forecast_Dashboard_with_Python_Part_1/img/cloud.png',
+                'Rain': 'Day_32_and_33_Build_a_Weather_Forecast_Dashboard_with_Python_Part_1/img/rain.png',
+                'Snow': 'Day_32_and_33_Build_a_Weather_Forecast_Dashboard_with_Python_Part_1/img/snow.png'
+            }
+            pictures = [sky_conditions[item] for item in data]
+            st.image(pictures, width=115)
+except KeyError:
+    st.error('Please enter a valid place')
