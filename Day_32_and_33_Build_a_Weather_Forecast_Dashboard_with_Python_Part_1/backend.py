f44b6fbedce4de39e04140b49826d9abbc9a1964
Alexander_Levkin <levkin.a@promail366.com>
2023-08-22 21:34:38 +0300

    add new 33 day

--- Day_32_and_33_Build_a_Weather_Forecast_Dashboard_with_Python_Part_1/backend.py
+++ Day_32_and_33_Build_a_Weather_Forecast_Dashboard_with_Python_Part_1/backend.py
@@ -7,6 +7,13 @@ def get_data(place, forecast_days=None, kind=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
+    filtered_data = data['list']
+    nr_values = 8*forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if kind == "Temperature":
+        data = [item['main']['temp'] for item in filtered_data]
+    elif kind == "Sky":
+        data = [item['weather'][0]['main'] for item in filtered_data]
     return data
 
 
