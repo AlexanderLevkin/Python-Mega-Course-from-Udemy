778163a33cd200d35a3e9e3893bed5e44f9ee4a6
Alexander_Levkin <levkin.a@promail366.com>
2023-08-24 21:17:45 +0300

    add new 34 day

--- Day_34_Natural_language_processing_of_books_with_python/book_analysis.py
+++ Day_34_Natural_language_processing_of_books_with_python/book_analysis.py
@@ -6,10 +6,16 @@ with open("../Day_34_Natural_language_processing_of_books_with_python/miracle_in
 chapter_count = book.count("Chapter")
 print(chapter_count)
 
-
 # with regex
 
 pattern = re.compile("Chapter [0-9]+")
 findings = re.findall(pattern, book)
 
+print(len(findings))
+
+# Which are the sentences that contain the word “Love”?
+
+pattern = re.compile('[A-Z]{1}[^.]*[^a-zA-Z]+love[^a-zA-Z][^.]*.')
+
+findings = re.findall(pattern, book)
 print(len(findings))
